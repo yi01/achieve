d6648d7fb7ef775f81a563a206ef217eda98a6d6
ikuzawa <chickforthe@yahoo.co.jp>
2017-03-20 03:53:21 +0000

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,56 @@
+class BlogsController < ApplicationController
+
+  before_action :set_blog, only: [:edit, :update, :destroy]  
+  
+  def index
+    @blogs = Blog.all
+    @contacts = Contact.all
+  end
+  
+  def new
+    if params[:back]
+      @blog = Blog.new(blogs_params)
+    else
+      @blog = Blog.new
+    end
+  end
+  
+  def create
+    @blog = Blog.new(blogs_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"    
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blogs_params)
+      redirect_to blogs_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました！"
+  end
+  
+  def confirm
+    @blog = Blog.new(blogs_params)
+    render 'new'  if @blog.invalid?
+  end
+  
+  private
+  def blogs_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+      @blog = Blog.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  resources :blogs, only: [:index, :new, :create, :edit, :update ,:destroy] do
+    collection do
+      post :confirm
+    end
+  end  
+  resources :contacts, only: [:new, :create] do
+    collection do 
+      post :confirm
+    end
+  end
+  
+  root 'top#index'
+end
