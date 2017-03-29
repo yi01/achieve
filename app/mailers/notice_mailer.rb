8cdb4d94d5a3a1eac48303e28f9b5815a45feff8
生澤祐平 <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-03-29 12:45:33 +0900

    email

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,14 +1,9 @@
 class NoticeMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notice_mailer.sendmail_blog.subject
-  #
-  def sendmail_blog
-    @blog = blog
+  def sendmail_blog(blog)
+    @greeting = "Hi"
 
-    mail to: "yi01234567890123456789@gmail.com"
-      subject: '【Achieve】ブログが投稿されました'
+    mail to: "自分のメールアドレス",
+         subject: '【Achieve】ブログが投稿されました'
   end
 end
